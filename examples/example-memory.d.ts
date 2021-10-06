88211e66fc80ff78702263ac87536e6718f79ca1
Mysak0CZ <michalmpacal@gmail.com>
2021-10-06 21:03:44 +0200

    Format rest

--- examples/example-memory.d.ts
+++ examples/example-memory.d.ts
@@ -1,35 +1,36 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 type CreepMemory = CreepMemory_startup_feeder | CreepMemory_startup_upgrader | CreepMemory_none;
 
 interface CreepMemory_startup_feeder {
-    role: "startup_feeder";
-    state: "harvest" | "feed";
-    source: string | null;
-    spawn: string | null;
+	role: "startup_feeder";
+	state: "harvest" | "feed";
+	source: string | null;
+	spawn: string | null;
 }
 
 interface CreepMemory_startup_upgrader {
-    role: "startup_upgrader";
-    state: "harvest" | "upgrade";
-    source: string | null;
+	role: "startup_upgrader";
+	state: "harvest" | "upgrade";
+	source: string | null;
 }
 
 interface CreepMemory_none {
-    role: "none";
+	role: "none";
 }
 
 interface Memory {
-    profiller: IProfillerMemory;
-    creeps: { [name: string]: CreepMemory };
-    flags: { [name: string]: FlagMemory };
-    rooms: { [name: string]: RoomMemory };
-    spawns: { [name: string]: SpawnMemory };
+	profiller: IProfillerMemory;
+	creeps: { [name: string]: CreepMemory };
+	flags: { [name: string]: FlagMemory };
+	rooms: { [name: string]: RoomMemory };
+	spawns: { [name: string]: SpawnMemory };
 }
 
 interface IProfillerMemory {
-    [symbol: string]: {
-        sum: number;
-        count: number;
-    };
+	[symbol: string]: {
+		sum: number;
+		count: number;
+	};
 }
 
 interface FlagMemory {}
